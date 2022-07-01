714f65330db69623e6073a0a5e2985d25289245e
hjyang <diva57855@gmail.com>
2022-07-02 01:41:18 +0900

    AUC-18 상품조회 MockAPI 공통코드 등 추가

--- src/main/java/kr/toyauction/domain/product/controller/ProductController.java
+++ src/main/java/kr/toyauction/domain/product/controller/ProductController.java
@@ -19,7 +19,6 @@ public class ProductController {
         String result = "{\n" +
                         "  \"success\": \"true\",\n" +
                         "  \"data\": {\n" +
-                        "    \"memberId\": 1,\n" +
                         "    \"productId\": 1,\n" +
                         "    \"images\": [\n" +
                         "      {\n" +
@@ -36,36 +35,54 @@ public class ProductController {
                         "      \"imageUrl\": \"https://example.com/IMG00002.png\"\n" +
                         "    },\n" +
                         "    \"productName\": \"Nike Air Zoom Iorem lorem lorem Pegasus 36 Miami\",\n" +
+                        "    \"maxBidPrice\": 40000,\n" +
                         "    \"minBidPrice\": 10000,\n" +
                         "    \"rightPrice\": 38000,\n" +
                         "    \"startSaleDateTime\": \"2022-06-19 21:48:55\",\n" +
                         "    \"endSaleDateTime\": \"2022-06-19 21:48:55\",\n" +
                         "    \"unitPrice\": 1000,\n" +
-                        "    \"purchaseTime\": \"6개월 이내\",\n" +
-                        "    \"deliveryOption\": \"직거래\",\n" +
-                        "    \"isExchange\": \"false\",\n" +
+                        "    \"purchaseTime\": {\n" +
+                        "      \"code\": \"PT_01\",\n" +
+                        "      \"name\": \"6개월 이내\"\n" +
+                        "    },\n" +
+                        "    \"deliveryOption\": {\n" +
+                        "      \"code\": \"DO_01\",\n" +
+                        "      \"name\": \"직거래\"\n" +
+                        "    },\n" +
+                        "    \"isExchange\": {\n" +
+                        "      \"code\": \"EC_01\",\n" +
+                        "      \"name\": \"교환 불가\"\n" +
+                        "    },\n" +
                         "    \"productCondition\": \"new\",\n" +
                         "    \"detail\": \"The nike Air max 270 React ENG combines a full-lengh React foam midsole with a 270 Max air unit for unrivaled comfor and striking visul experience. The nike Air max 270 React ENG combines a full-lengh React foam midsole with a 270 Max air unit for unrivaled comfor and striking visul experience. The nike Air max 270 React ENG combines a full-lengh React foam midsole with a 270 Max air unit for unrivaled comfor and striking visul experience. The nike Air max 270 React ENG combines a full-lengh React foam midsole with a 270 Max air unit for unrivaled comfor and striking visul experience.\",\n" +
+                        "    \"bidCount\": 3,\n" +
                         "    \"bids\": [\n" +
                         "      {\n" +
-                        "        \"bidId\": \"1\",\n" +
+                        "        \"bidId\": 1,\n" +
+                        "        \"bidSeq\": 1,\n" +
                         "        \"bidPrice\": 30000,\n" +
                         "        \"bidDateTime\": \"2022-06-19 21:48:55\"\n" +
                         "      },\n" +
                         "      {\n" +
-                        "        \"bidId\": \"2\",\n" +
+                        "        \"bidId\": 2,\n" +
+                        "        \"bidSeq\": 2,\n" +
                         "        \"bidPrice\": 10000,\n" +
                         "        \"bidDateTime\": \"2022-06-19 21:48:55\"\n" +
                         "      },\n" +
                         "      {\n" +
-                        "        \"bidId\": \"3\",\n" +
+                        "        \"bidId\": 3,\n" +
+                        "        \"bidSeq\": 3,\n" +
                         "        \"bidPrice\": 40000,\n" +
                         "        \"bidDateTime\": \"2022-06-19 21:48:55\"\n" +
                         "      }\n" +
                         "    ],\n" +
+                        "    \"registerMemberId\": 1,\n" +
+                        "    \"productSttus\": {\n" +
+                        "      \"code\": \"ST_01\",\n" +
+                        "      \"name\": \"판매중\"\n" +
+                        "    },\n" +
                         "    \"createDatetime\": \"2022-06-19 21:48:55\",\n" +
-                        "    \"updateDatetime\": \"2022-06-19 21:48:55\",\n" +
-                        "    \"enabled\": \"true\"\n" +
+                        "    \"updateDatetime\": \"2022-06-19 21:48:55\"\n" +
                         "  }\n" +
                         "}";
 
