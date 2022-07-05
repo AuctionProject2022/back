ca2ed6dcd63c9a6d0b6ce64bc1de31f898fdc7cc
hjyang <diva57855@gmail.com>
2022-07-05 22:44:37 +0900

    AUC-19 Bid 도메인 제거, Product에 merge

--- src/main/java/kr/toyauction/domain/product/controller/ProductController.java
+++ src/main/java/kr/toyauction/domain/product/controller/ProductController.java
@@ -109,4 +109,18 @@ public class ProductController {
 
         return result;
     }
+
+    @PostMapping(ProductPath.PRODUCTS + "/bids")
+    public String postBid() {
+        String result = "{\n" +
+                "  \"success\": \"true\",\n" +
+                "  \"data\": {\n" +
+                "    \"bidId\": 1,\n" +
+                "    \"createDatetime\": \"2022-06-19 21:48:55\",\n" +
+                "    \"updateDatetime\": \"2022-06-19 21:48:55\"\n" +
+                "  }\n" +
+                "}";
+
+        return result;
+    }
 }
