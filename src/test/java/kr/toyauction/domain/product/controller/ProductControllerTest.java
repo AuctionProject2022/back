30118654a323cc9ca514557068e52440a17ff93f
gumicode <gumicode@gmail.com>
2022-07-09 00:01:18 +0900

    AUC-40 feat: spring rest docs get product example 코드 추가

--- src/test/java/kr/toyauction/domain/product/controller/ProductControllerTest.java
+++ src/test/java/kr/toyauction/domain/product/controller/ProductControllerTest.java
@@ -4,27 +4,29 @@ import kr.toyauction.domain.product.property.ProductPath;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import static org.springframework.restdocs.headers.HeaderDocumentation.*;
-import static org.springframework.restdocs.hypermedia.HypermediaDocumentation.*;
+import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
+import static org.springframework.restdocs.headers.HeaderDocumentation.responseHeaders;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
-import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.relaxedResponseFields;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
-@ExtendWith({RestDocumentationExtension.class, SpringExtension.class})
+@ExtendWith({RestDocumentationExtension.class})
+@AutoConfigureRestDocs(uriScheme = "https", uriHost = "api.toyauction.kr")
 class ProductControllerTest {
 
 	private MockMvc mockMvc;
@@ -43,19 +45,42 @@ class ProductControllerTest {
 				.andDo(print())
 				.andExpect(status().isOk())
 				.andDo(document("get-product",
-//						links(
-//								linkWithRel("self").description("link to self"),
-//								linkWithRel("query-events").description("link to query events"),
-//								linkWithRel("update-event").description("link to update an existing"),
-//								linkWithRel("profile").description("profile")
-//						),
 						responseHeaders(
 								headerWithName(HttpHeaders.CONTENT_TYPE).description("Content Type")
 						),
-						//relaxedResponseFields : 문서에 일부분을 확인한다.
-						//responseFields : 모든 요소가 다 들어가 있어야 한다.
 						relaxedResponseFields(
-								fieldWithPath("data.productId").description("id")
+								fieldWithPath("data.productId").description("상품 고유번호"),
+								fieldWithPath("data.images[].fileId").description("상품 이미지 파일번호"),
+								fieldWithPath("data.images[].fileType").description("상품 이미지 파일타입"),
+								fieldWithPath("data.images[].url").description("상품 이미지 경로"),
+								fieldWithPath("data.thumbnailImage.fileId").description("상품 썸네일 이미지 파일번호"),
+								fieldWithPath("data.thumbnailImage.fileType").description("상품 썸네일 이미지 파일타입"),
+								fieldWithPath("data.thumbnailImage.url").description("상품 썸네일 이미지 경로"),
+								fieldWithPath("data.productName").description("상품 이름"),
+								fieldWithPath("data.maxBidPrice").description("즉시 구매가"),
+								fieldWithPath("data.minBidPrice").description("최초 입찰 시작가"),
+								fieldWithPath("data.rightPrice").description("현재 입찰가"),
+								fieldWithPath("data.startSaleDateTime").description("판매 시작 기간"),
+								fieldWithPath("data.endSaleDateTime").description("판매 종료 기간"),
+								fieldWithPath("data.unitPrice").description("입찰 단위"),
+								fieldWithPath("data.purchaseTime.code").description("구매 시간 코드"),
+								fieldWithPath("data.purchaseTime.name").description("구매 시간 내용"),
+								fieldWithPath("data.deliveryOption.code").description("배송 옵션 코드"),
+								fieldWithPath("data.deliveryOption.name").description("배송 옵션 내용"),
+								fieldWithPath("data.isExchange.code").description("교환가능 코드"),
+								fieldWithPath("data.isExchange.name").description("교환가능 내용"),
+								fieldWithPath("data.productCondition").description("상품 상태"),
+								fieldWithPath("data.detail").description("상품 내용"),
+								fieldWithPath("data.bidCount").description("총 입찰 수"),
+								fieldWithPath("data.bids[].bidId").description("입찰 번호"),
+								fieldWithPath("data.bids[].bidSeq").description("입찰 순서"),
+								fieldWithPath("data.bids[].bidPrice").description("입찰 금액"),
+								fieldWithPath("data.bids[].bidDateTime").description("입찰 시간"),
+								fieldWithPath("data.registerMemberId").description("등록자 회원번호"),
+								fieldWithPath("data.productSttus.code").description("판매 상태 코드"),
+								fieldWithPath("data.productSttus.name").description("판매 상태 이름"),
+								fieldWithPath("data.createDatetime").description("등록일"),
+								fieldWithPath("data.updateDatetime").description("수정일")
 						)
 				));
 	}
